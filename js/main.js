b51b1f627db5ad1a00ffa2c0b720a025f8df9b45
Abby Mueller <68559641+amueller8@users.noreply.github.com>
2023-02-20 13:08:23 -0500

    index and basic js for flavor toggle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 // JavaScript is similar to Java...but also very different.
-console.log("Hello, world!");
-confirm("Can you read this?");
+
+let toggle = 0;
+
+function mainClick(){
+    document.getElementsByClassName('main-content')[0].style.visibility = 'visible';
+    document.getElementsByClassName('flavors')[0].style.visibility = 'hidden';
+}
+
+
+function flavorClick(){
+    document.getElementsByClassName('main-content')[0].style.visibility = 'hidden';
+    document.getElementsByClassName('flavors')[0].style.visibility = 'visible';
+
+
+}
